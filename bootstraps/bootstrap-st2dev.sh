1a3c9a57462ec5266bbf3538be0ff71b38ea9a99
W Chan <m4d.coder@gmail.com>
2016-03-01 14:56:51 -0800

    Add ssh system user creation to bootstrap script

--- bootstraps/bootstrap-st2dev.sh
+++ bootstraps/bootstrap-st2dev.sh
@@ -22,3 +22,16 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get -y update
 apt-get install -y mongodb-org
+
+# StackStorm SSH system user
+useradd stanley
+mkdir -p /home/stanley/.ssh
+chmod 0700 /home/stanley/.ssh
+ssh-keygen -f /home/stanley/.ssh/stanley_rsa -P ""
+sh -c 'cat /home/stanley/.ssh/stanley_rsa.pub >> /home/stanley/.ssh/authorized_keys'
+chmod 0600 /home/stanley/.ssh/authorized_keys
+chown -R stanley:stanley /home/stanley
+sh -c 'echo "stanley    ALL=(ALL)       NOPASSWD: SETENV: ALL" >> /etc/sudoers.d/st2'
+chmod 0440 /etc/sudoers.d/st2
+cp /home/stanley/.ssh/stanley_rsa /home/vagrant/.ssh/stanley_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/stanley_rsa
