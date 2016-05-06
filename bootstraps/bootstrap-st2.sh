7501a8073213b106b177e395bc169091ec43a742
W Chan <m4d.coder@gmail.com>
2016-05-05 18:03:44 -0700

    Add script to install st2 from packages

---
+++ bootstraps/bootstrap-st2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+if [ `uname` != "Linux" ]; then
+    echo "ERROR: bootstrap.sh is only supported on a Linux system."
+    exit 1
+fi
+
+ST2_BOOTSTRAP="/tmp/st2_bootstrap.sh"
+
+wget -O ${ST2_BOOTSTRAP} https://raw.githubusercontent.com/StackStorm/st2-packages/master/scripts/st2_bootstrap.sh
+chmod +x ${ST2_BOOTSTRAP}
+bash ${ST2_BOOTSTRAP} --stable --user=admin --password=StackSt0rm
+usermod -a -G st2packs vagrant
