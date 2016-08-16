e4412c15746045457a44815a5eedf1919e9a5bfd
W Chan <m4d.coder@gmail.com>
2016-08-16 15:16:11 -0700

    Change MongoDB to version 3.2

--- bootstraps/bootstrap-st2dev.sh
+++ bootstraps/bootstrap-st2dev.sh
@@ -24,9 +24,9 @@ service rabbitmq-server restart
 curl -sS -o /usr/bin/rabbitmqadmin http://127.0.0.1:15672/cli/rabbitmqadmin
 chmod 755 /usr/bin/rabbitmqadmin
 
-# MongoDB 3.x
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+# MongoDB 3.2
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get -y update
 apt-get install -y mongodb-org
 
