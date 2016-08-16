9542dd4491594178a938b8e220ffe5aecb128d15
W Chan <m4d.coder@gmail.com>
2016-08-16 15:15:38 -0700

    Create .vimrc file

--- bootstraps/bootstrap.sh
+++ bootstraps/bootstrap.sh
@@ -13,3 +13,20 @@ apt-get -y dist-upgrade
 apt-get -y install ${DEVKIT} ${TOOLS}
 apt-get clean all
 apt-get autoremove
+
+VIMRC_FILE=~/.vimrc
+cat <<VIMRC_OPTS >$VIMRC_FILE
+set shiftwidth=4
+set tabstop=4
+set expandtab
+set softtabstop=4
+set smarttab
+set backspace=indent,eol,start
+set nocompatible
+set paste
+set ruler
+syntax on
+filetype plugin on
+highlight OverLength ctermbg=red ctermfg=white guibg=#592929
+match OverLength /\%81v.\+/
+VIMRC_OPTS
