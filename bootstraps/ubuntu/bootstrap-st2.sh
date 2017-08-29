6015ba8b743a8bed849f53070683201d8fb578a8
W Chan <m4d.coder@gmail.com>
2017-08-29 14:41:34 -0700

    Update bootstrap-st2 to install staging stable

--- bootstraps/ubuntu/bootstrap-st2.sh
+++ bootstraps/ubuntu/bootstrap-st2.sh
@@ -10,5 +10,5 @@ ST2_BOOTSTRAP="/tmp/st2_bootstrap.sh"
 
 wget -O ${ST2_BOOTSTRAP} https://raw.githubusercontent.com/StackStorm/st2-packages/master/scripts/st2_bootstrap.sh
 chmod +x ${ST2_BOOTSTRAP}
-bash ${ST2_BOOTSTRAP} --stable --user=admin --password=StackSt0rm
+bash ${ST2_BOOTSTRAP} --staging --stable --user=admin --password=StackSt0rm
 usermod -a -G st2packs vagrant
